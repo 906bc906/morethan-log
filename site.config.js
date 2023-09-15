1fd97f11e8aa956041159e043c5f9e12d47b5cf4
906bc <906bc906@gmail.com>
2023-09-16 01:13:29 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,21 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "906bc906",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "role",
+    bio: "bio",
+    email: "906bc906@gmail.com",
+    linkedin: "",
+    github: "906bc906",
     instagram: "",
   },
   projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
-    },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "906bc906's morethan-log",
+    description: "powered by morethan-log",
   },
 
   // CONFIG configration (required)
@@ -47,7 +43,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
